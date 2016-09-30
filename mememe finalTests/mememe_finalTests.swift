14dbeb3f4328b24bcfd9a19d7334478bf8d86d17
mr-prabhjot <prabhjottechs@gmail.com>
2016-09-30 11:06:28 +0530

    Initial Commit

---
+++ mememe finalTests/mememe_finalTests.swift
@@ -0,0 +1,36 @@
+//
+//  mememe_finalTests.swift
+//  mememe finalTests
+//
+//  Created by Prabhjot  on 30/09/16.
+//  Copyright © 2016 Prabhjot . All rights reserved.
+//
+
+import XCTest
+@testable import mememe_final
+
+class mememe_finalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
